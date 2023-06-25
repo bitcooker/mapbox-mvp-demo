ec04aaadc8ed617bdac3830a1023c65bf9ce6315
bitcooker <dreamhighpassionhigh@gmail.com>
2023-06-25 20:16:41 +0800

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>
+      <body className='flex flex-col min-h-[100vh]'>
         <Header />
         {children}
       </body>
