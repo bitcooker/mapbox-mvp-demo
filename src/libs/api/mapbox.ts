209211437be24f0e9c8f710863ce4ca3bb7ca477
bitcooker <dreamhighpassionhigh@gmail.com>
2023-06-26 04:43:18 +0800

    feat: get features from coords

---
+++ src/libs/api/mapbox.ts
@@ -0,0 +1,7 @@
+import { LngLat } from 'mapbox-gl'
+
+export const getFeaturesFromCoords = async (_coords: LngLat) => {
+    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${_coords.lng},${_coords.lat}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
+    const response = await fetch(apiUrl);
+    return await response.json();
+}
